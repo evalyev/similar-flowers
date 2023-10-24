f0c0c985b2816b1f2a5ab92d698f9460319ed5df
Евгений Бакаев <bak.evsha@gmail.com>
2023-10-24 23:50:02 +0300

    Добавлена возможность визуализировать похожие фото

--- frontend/src/components/Main/Main.tsx
+++ frontend/src/components/Main/Main.tsx
@@ -10,15 +10,14 @@ import './Main.scss'
 export default function Main(methods: any) {
   const toast = useRef<any>(null);
   const [file, setFile] = useState<any>(null)
-
-  const onUpload = () => {
-    toast.current.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' });
-    console.log(toast.current)
-  };
+  const [isVisibleContent, setIsVisibleContent] = useState(false)
+  const [images, setImages] = useState<any>(null)
 
   const onSelect = (evt: any) => {
     console.log(evt.files[0])
     setFile(evt.files[0])
+    setIsVisibleContent(false)
+    setImages(null)
   }
 
   const onClear = () => {
@@ -29,6 +28,14 @@ export default function Main(methods: any) {
     evt.preventDefault()
     console.log('test')
     if (!file) return;
+    setIsVisibleContent(true)
+    toast.current.show({ severity: 'info', summary: 'Success', detail: 'Загружаются похожие фото' });
+  }
+
+  function handleRandomClick() {
+    setFile(null)
+    setIsVisibleContent(false)
+    setImages(null)
   }
 
   return (
@@ -38,14 +45,21 @@ export default function Main(methods: any) {
       <form className='form' name='load-files' onSubmit={handleSubmit} encType='multipart/form-data'>
         <div className="form_files card flex justify-content-center">
           <Toast ref={toast}></Toast>
-          <FileUpload mode="basic" name="upload-file" accept="image/*" maxFileSize={1000000} onUpload={onUpload} onSelect={onSelect}
+          <FileUpload mode="basic" name="upload-file" accept="image/*" maxFileSize={1000000} onSelect={onSelect}
             onClear={onClear} chooseLabel='Выбрать' className='form_btn' />
+          <Button label="Рандомная фотка" icon="pi pi-search" iconPos="right" className='form_btn' onClick={handleRandomClick} />
           <Button label="Отправить" icon="pi pi-check" iconPos="right" type='submit' />
         </div>
       </form>
 
       {file && (<img src={file.objectURL} alt='Картинка цветка' />)}
 
+
+      {isVisibleContent && (
+        <div className='imgs'>
+          {images.map((image_name: any) => <img className='imgs_img' src={require(`../../images/${image_name}`)} />)}
+        </div>
+      )}
     </main>
   )
 }
